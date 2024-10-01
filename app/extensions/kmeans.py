06db7e666b53fdec0a02a9dbbd1b47acbe007086
Jose <josev814@gmail.com>
2024-10-01 13:52:10 -0400

    adding requirements file for docker (#4)
    also adding kmeans tasks

---
+++ app/extensions/kmeans.py
@@ -0,0 +1,16 @@
+### create kmeans process here
+import pandas
+"""
+    Task 1:
+    Implement a clustering algorithm to group similar code submissions using an ML method like k-means.
+    This will help identify clusters of highly similar code for further investigation.
+"""
+
+
+"""
+    Task 5:
+    If we were to create a graph that shows each submitted code
+    as a point with clusters plotted in the XY plane,
+    what are your thoughts and ideas on how we should approach this visualization?
+    Any suggestions on useful metrics or dimensions to represent?
+"""
